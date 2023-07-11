aa66716672aff7791463cd9612ed9337f4ade2dc
Csaba <not@public.com>
2023-07-11 21:00:38 +0200

    testing tailwindcss

--- src/components/Sections/Footer/Footer.tsx
+++ src/components/Sections/Footer/Footer.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Footer.css'
 
+
+
 function Footer(){
         return (
-            <div className="Footer">
-                <div>© Copyright 2023 Csaba Andi</div>
-            </div>
+           
+            <footer className="w-50">
+              <div className="flex justify-center bg-slate-300">© 2023 Test</div>
+            </footer>
             );
     }
 
 
 export default Footer;
+
+/*<div className="Footer">
+                <div>© Copyright 2023 Csaba Andi</div>
+            </div>*/
