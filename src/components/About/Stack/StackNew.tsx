942ecc3aac2ca7aec32a197e5fc92ba6ad87c128
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-03-10 23:24:12 +0100

    [Code] added <abbr> tag to StackPartBlock() for showing a description text on mouse hover

--- src/components/About/Stack/StackNew.tsx
+++ src/components/About/Stack/StackNew.tsx
@@ -2,12 +2,16 @@ var bg_color_skill_full = "bg-amber-400"
 var bg_color_skill_empty = "bg-stone-700"
 
 
-const StackPartBlock: React.FC<{ name: string }> = ({ name }) => (
-    <div className='class="flex flex-col items-center justify-center space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
-        <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{name}</h3> {/* Python Java JavaScript / TypeScript */}
+const StackPartBlock: React.FC<{ name: string, description: string}> = ({ name, description}) => (
+    <div>
+        <abbr style={{border: "none", textDecoration: "none"}} title={description}>
+            <div className='class="flex flex-col items-center justify-center space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
+                <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{name}</h3> {/* Python Java JavaScript / TypeScript */}
+            </div>
+        </abbr>
     </div>
 );
-  
+
 
 function StackNew() {
     return (
@@ -19,27 +23,28 @@ function StackNew() {
             <div className='grid grid-cols-2 gap-2 sm:auto-cols-fr sm:grid-flow-col lg:gap-8'>
                 <div className='class="flex flex-col items-center justify-center space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
                     <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>Backend</h3> {/* Python Java JavaScript / TypeScript */}
-                    <StackPartBlock name="Docker"></StackPartBlock>  
-                    <StackPartBlock name="Docker"></StackPartBlock>  
-                    <StackPartBlock name="Docker"></StackPartBlock>
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>  
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>  
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>
                 </div>
                 <div className='class="flex flex-col items-center justify-center space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
                     <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>Frontend</h3> {/* Python Java JavaScript / TypeScript */}
-                    <StackPartBlock name="Docker"></StackPartBlock>  
-                    <StackPartBlock name="Docker"></StackPartBlock>  
-                    <StackPartBlock name="Docker"></StackPartBlock>
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>  
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>  
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>
                 </div>
                 <div className='class="flex flex-col items-center justify-center space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
                     <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>Contanirezation</h3> {/* Python Java JavaScript / TypeScript */}
-                    <StackPartBlock name="Docker"></StackPartBlock>  
-                    <StackPartBlock name="Docker"></StackPartBlock>  
-                    <StackPartBlock name="Docker"></StackPartBlock>
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>  
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>  
+                    <StackPartBlock name="Docker" description=""></StackPartBlock>
                 </div>
+                {/**  @todo: delete unnecessary words**/}
                 <div className='class="flex flex-col items-center justify-center space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
                     <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>Engineering</h3> {/* Python Java JavaScript / TypeScript */}
-                    <StackPartBlock name="Docker"></StackPartBlock>  
-                    <StackPartBlock name="Docker"></StackPartBlock>  
-                    <StackPartBlock name="Docker"></StackPartBlock>
+                    <StackPartBlock name="Elektronika" description="elektrooo"></StackPartBlock>  
+                    <StackPartBlock name="Mikrovezérlők" description="miki"></StackPartBlock>  
+                    <StackPartBlock name="Irányításteknika(?)" description=""></StackPartBlock>
                 </div>
                 
             </div>
