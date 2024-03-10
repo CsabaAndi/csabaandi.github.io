0bbde8033a3f583cc87776b3027c3b345f79af9c
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-03-10 13:40:23 +0100

    [Refactor] refactored deprecated function

--- src/components/BackToTop/Back.tsx
+++ src/components/BackToTop/Back.tsx
@@ -9,7 +9,7 @@ function BackToTop() {
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window.pageYOffset > 300) {
+      if (window.scrollY > 300) {
         setShowButton(true);
       } else {
         setShowButton(false);
