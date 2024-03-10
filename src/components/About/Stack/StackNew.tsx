15a7407125d5caa2cad7264a27fa5b3b859efbce
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-03-10 23:24:59 +0100

    comment

--- src/components/About/Stack/StackNew.tsx
+++ src/components/About/Stack/StackNew.tsx
@@ -4,7 +4,7 @@ var bg_color_skill_empty = "bg-stone-700"
 
 const StackPartBlock: React.FC<{ name: string, description: string}> = ({ name, description}) => (
     <div>
-        <abbr style={{border: "none", textDecoration: "none"}} title={description}>
+        <abbr style={{border: "none", textDecoration: "none"}} title={description}> {/**  @todo better implementation for desc showing **/}
             <div className='class="flex flex-col items-center justify-center space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
                 <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{name}</h3> {/* Python Java JavaScript / TypeScript */}
             </div>
