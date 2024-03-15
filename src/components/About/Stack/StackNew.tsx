c7654276d1a1b366174aeed30b5f03d0cd190901
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-03-16 00:14:23 +0100

    [tailwindcss] removed borders | adjusted gap sizes

--- src/components/About/Stack/StackNew.tsx
+++ src/components/About/Stack/StackNew.tsx
@@ -1,6 +1,7 @@
 import { PythonIcon } from "../../Icons/ProjectIcons";
 
 
+{/** @todo: delete **/}
 var bg_color_skill_full = "bg-amber-400"
 var bg_color_skill_empty = "bg-stone-700"
 
@@ -8,70 +9,72 @@ var bg_color_skill_empty = "bg-stone-700"
 const StackPartBlock: React.FC<{ name: string, description: string}> = ({ name, description}) => (
     <div>
         <abbr style={{border: "none", textDecoration: "none"}} title={description}> {/**  @todo better implementation for desc showing **/}
-            <div className='flex flex-col items-center justify-center space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
+            <div className='flex flex-col items-center justify-center space-y-2 rounded-xl border-none  bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
                 <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{name}</h3> {/* Python Java JavaScript / TypeScript */}
             </div>
         </abbr>
     </div>
-);
+)
 
+
+{/** @todo: fix gap-size **/}
 const TestBlock: React.FC<{ title: string}> = ({ title}) => (
     <div className='p-2'>
         <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{title}</h3> {/* Python Java JavaScript / TypeScript */}
         <div className='grid grid-rows-1 border-none'>
-            <div className='flex flex-wrap gap-2
-                            justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+            <div className='flex flex-wrap gap-4 md:gap-8 lg:gap-9
+                            justify-items-start items-center rounded-xl border-none  bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
                             shadow-xl shadow-black/30'>
 
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
-                <div className='border-2 border-green-900 bg-stone-800'>
+                <div className='border-none  bg-stone-800'>
                     <PythonIcon size_min={16} size_standard={32}></PythonIcon>
                 </div>
             </div>
@@ -89,7 +92,7 @@ function StackNew() {
             </h2>
 
             <div className='grid'>
-                <div className='space-y-2 rounded-xl border-none border-green-900 bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
+                <div className='space-y-2 rounded-xl border-none  px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
                     <TestBlock title="Backend"></TestBlock>
                     <TestBlock title="Frontend"></TestBlock>
                     <TestBlock title="Privacy"></TestBlock>
