eb64fa40bc6ece06cffbb0f754608ca9c5240a61
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-06 18:53:53 +0200

    [UI] Padding changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,9 +7,9 @@ import { EmailIcon, GithubIcon, LinkedinIcon } from "../Icons/SocialIcons"
 
 function Footer(){
         return (
-          <footer className='flex flex-col w-full items-center justify-between gap-y-4 border-t-4 border border-stone-900 bg-stone-800 px-4 py-6 sm:flex-row sm:py-4 md:px-8'>
-          <p id="footer_p_copyright" className='w-max text-sm font-semibold text-stone-300'>© 2023 Csaba Andi</p>
-          <div id="footer_icons" className='flex gap-x-6 sm:gap-x-4 text-stone-200'>
+          <footer className='flex flex-col w-full items-center justify-between gap-y-2 bg-stone-800 py-2 sm:p-4 sm:flex-row'>
+          <p id="footer_p_copyright" className='text-sm font-semibold text-stone-300'>© 2023 Csaba Andi</p>
+          <div id="footer_icons" className='flex gap-x-2 sm:gap-x-4 text-stone-200'>
             <GithubIcon size_min={20} size_standard={20}></GithubIcon>
             <LinkedinIcon size_min={20} size_standard={20}></LinkedinIcon>
             {/** @todo: better implementation pls **/}
