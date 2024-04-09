06f01f74e9f4f0d1afdecb6b6fb2d2debd6964f7
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-09 17:37:38 +0200

    [UI] Changed icons to new proper icons | imported all icons

--- src/components/About/Thesis.tsx
+++ src/components/About/Thesis.tsx
@@ -1,14 +1,15 @@
 import {GithubIcon} from '../Icons/SocialIcons'
-import { PythonIcon } from "../Icons/ProjectIcons"
+import * as Icons from '../Icons/ProjectIcons'
 
 
-const Test: React.FC<{ name: string}> = ({ name }) => (
+{/** @todo: whole block shenanigans */}
+const Testt: React.FC<{ name: string}> = ({ name}) => ( 
     <div className='grid grid-cols-2  sm:visible col-span-2 col-start-2 sm:col-start-1 sm:col-span-6 md:col-start-1 md:col-span-8 lg:col-start-2 lg:col-span-8 
                     justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
                     shadow-xl shadow-black/30 sm:p-6 gap-4  sm:gap-10'>
         <h3 className='text-tiny sm:text-xs text-center font-extrabold tracking-tight break-normal text-stone-200 lg:text-md border-none border-green-900 bg-stone-800'>{name}</h3>
-        <div className='border-none border-green-900 bg-stone-800'>
-            <PythonIcon size_min={16} size_standard={32}></PythonIcon>
+        <div className='bg-stone-800'>
+            <Icons.PythonIcon size_min={16} size_standard={32}></Icons.PythonIcon>
         </div>
     </div>
 )
@@ -32,24 +33,51 @@ function Thesis() {
                             
                             </h3>
                             <div className='p-1 ml-2 lg:ml-16 md:ml-10 sm:ml-2 sm:pr-1 md:p-1 border-none border-green-900'>
-                                <GithubIcon size_min={24} size_standard={32}></GithubIcon>
+                                <GithubIcon size_min={24} size_standard={32}></GithubIcon>`
                             </div>
 
                         </div>
                         <div className='grid grid-cols-4 gap-x-2'>
-                            <div className='flex flex-col col-start-1 place-content-evenly gap-y-2'>
-                                <div className=''>
-                                    <Test name="Python"></Test>
+                            <div className='flex flex-col col-start-1 place-content-evenly gap-y-2'> {/** @todo: whole block shenanigans */}
+                                
+
+                            <div className='grid grid-cols-2  sm:visible col-span-2 col-start-2 sm:col-start-1 sm:col-span-6 md:col-start-1 md:col-span-8 lg:col-start-2 lg:col-span-8 
+                                        justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                                        shadow-xl shadow-black/30 sm:p-6 gap-4  sm:gap-10'>
+                                <h3 className='text-tiny sm:text-xs text-center font-extrabold tracking-tight break-normal text-stone-200 lg:text-md border-none border-green-900 bg-stone-800'>Python</h3>
+                                <div className='bg-stone-800'>
+                                    <Icons.PythonIcon size_min={16} size_standard={32}></Icons.PythonIcon>
                                 </div>
-                                <div className=''>
-                                    <Test name="Numpy"></Test>
+                            </div>
+
+                            <div className='grid grid-cols-2  sm:visible col-span-2 col-start-2 sm:col-start-1 sm:col-span-6 md:col-start-1 md:col-span-8 lg:col-start-2 lg:col-span-8 
+                                        justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                                        shadow-xl shadow-black/30 sm:p-6 gap-4  sm:gap-10'>
+                                <h3 className='text-tiny sm:text-xs text-center font-extrabold tracking-tight break-normal text-stone-200 lg:text-md border-none border-green-900 bg-stone-800'>Numpy</h3>
+                                <div className='bg-stone-800'>
+                                    <Icons.NumpyIcon size_min={16} size_standard={32}></Icons.NumpyIcon>
                                 </div>
-                                <div className=''>
-                                    <Test name="Tensorflow"></Test>
+                            </div>
+
+                            <div className='grid grid-cols-2  sm:visible col-span-2 col-start-2 sm:col-start-1 sm:col-span-6 md:col-start-1 md:col-span-8 lg:col-start-2 lg:col-span-8 
+                                        justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                                        shadow-xl shadow-black/30 sm:p-6 gap-4  sm:gap-10'>
+                                <h3 className='text-tiny sm:text-xs text-center font-extrabold tracking-tight break-normal text-stone-200 lg:text-md border-none border-green-900 bg-stone-800'>Tensorflow</h3>
+                                <div className='bg-stone-800'>
+                                    <Icons.TensorFlowIcon size_min={16} size_standard={32}></Icons.TensorFlowIcon>
                                 </div>
-                                <div className=''>
-                                    <Test name="Pandas"></Test>
+                            </div>
+
+                            <div className='grid grid-cols-2 sm:visible col-span-2 col-start-2 sm:col-start-1 sm:col-span-6 md:col-start-1 md:col-span-8 lg:col-start-2 lg:col-span-8 
+                                        justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                                        shadow-xl shadow-black/30 sm:p-6 gap-4  sm:gap-10'>
+                                <h3 className='text-tiny sm:text-xs text-center font-extrabold tracking-tight break-normal text-stone-200 lg:text-md border-none border-green-900 bg-stone-800'>Pandas</h3>
+                                <div className='bg-stone-800'>
+                                    <Icons.PandasIcon size_min={16} size_standard={32}></Icons.PandasIcon>
                                 </div>
+                            </div>
+                                
+
                             </div>
                             <div className='grid grid-rows-5 col-start-2 col-span-3 overflow-hidden border-none border-green-900 bg-stone-900 rounded-xl'>  {/** @todo: cols/rows--> size sm-md-lg **/}
                                 <p className='row-span-5 break-all text-md text-left tracking-tight text-stone-200 sm:text-lg sm:px-2 sm:py-4 shadow-xl shadow-black/30'> {/** @todo: word break: break-all for now **/}
