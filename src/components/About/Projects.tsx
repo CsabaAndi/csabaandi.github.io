7a1a69ac57947294674a958975b19bd4f4260c05
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-10 22:09:36 +0200

    [New] Added hidden project block for future use

--- src/components/About/Projects.tsx
+++ src/components/About/Projects.tsx
@@ -115,6 +115,50 @@ function Projects() {
                 </div>
               </div>
 
+
+              <div className='flex flex-col hiddenspace-y-2 p-2 pb-4 rounded-xl border-2 sm:border-none border-green-900 sm:bg-stone-800 shadow-xl shadow-black/30 sm:p-6 md:p-4 lg:p-4'>
+                <h3 className='text-md text-center mb-2 font-extrabold tracking-tight text-stone-200 sm:text-lg'>LOREM</h3>
+                {/** @todo: whole block shenanigans */}
+                <div className='grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10'>
+                  <div className='grid grid-cols-2 col-span-2 col-start-2 sm:col-start-1 sm:col-span-6 md:col-start-1 md:col-span-8 lg:col-start-2 lg:col-span-8 
+                                  justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                                  shadow-xl shadow-black/30 sm:p-6 gap-10  sm:gap-10'>
+                    <h3 className='text-xs text-center font-extrabold tracking-tight text-stone-200 lg:text-md border-none border-green-900 bg-stone-800'>LOREM</h3>
+                    <div className='border-none border-green-900 bg-stone-800'>
+                      <Icons.ReactIcon size_min={16} size_standard={32}></Icons.ReactIcon>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10'>
+                  <div className='grid grid-cols-2 col-span-2 col-start-2 sm:col-start-1 sm:col-span-6 md:col-start-1 md:col-span-8 lg:col-start-2 lg:col-span-8 
+                                  justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                                  shadow-xl shadow-black/30 sm:p-6 gap-10  sm:gap-10'>
+                    <h3 className='text-xs text-center font-extrabold tracking-tight text-stone-200 lg:text-md border-none border-green-900 bg-stone-800'>LOREM</h3>
+                    <div className='border-none border-green-900 bg-stone-800'>
+                      <Icons.TailwindIcon size_min={16} size_standard={32}></Icons.TailwindIcon>
+                    </div>
+                  </div>
+                </div>
+                
+                <div className='grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10'>
+                  <div className='grid grid-cols-2 col-span-2 col-start-2 sm:col-start-1 sm:col-span-6 md:col-start-1 md:col-span-8 lg:col-start-2 lg:col-span-8 
+                                  justify-items-start items-center rounded-xl border-none border-green-900 bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                                  shadow-xl shadow-black/30 sm:p-6 gap-10  sm:gap-10 mb-2'>
+                    <h3 className='text-xs text-center font-extrabold tracking-tight text-stone-200 lg:text-md border-none border-green-900 bg-stone-800'>LOREM</h3>
+                    <div className='border-none border-green-900 bg-stone-800'>
+                      <Icons.DockerIcon size_min={16} size_standard={32}></Icons.DockerIcon>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="grid h-full justify-center content-end overflow-hidden border-none border-green-900 bg-none rounded-xl">
+                  <p className='row-span-5 text-md text-left tracking-tight text-stone-200 sm:text-lg sm:px-2 sm:py-4 shadow-xl shadow-black/30'> {/** @todo: word break: break-all for now **/}
+                    <h3 className='text-md break-normal text-center font-extrabold tracking-tight text-stone-200 px-2 sm:text-lg'>LOREM</h3>
+                  </p>
+                </div>
+              </div>
+
             </div>
 
           </div> 
