bbcdafaeafcff6f40f1a4331218ff62739c0ba85
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-10 21:40:02 +0200

    stupid me deleted a few commits and now has to deal with merge conflict

--- src/components/About/Stack/StackNew.tsx
+++ src/components/About/Stack/StackNew.tsx
@@ -1,4 +1,4 @@
-import { PythonIcon } from "../../Icons/ProjectIcons";
+import * as Icons from "../../Icons/ProjectIcons"
 
 
 {/** @todo: delete **/}
@@ -6,81 +6,71 @@ var bg_color_skill_full = "bg-amber-400"
 var bg_color_skill_empty = "bg-stone-700"
 
 
-const StackPartBlock: React.FC<{ name: string, description: string}> = ({ name, description}) => (
-    <div>
-        <abbr style={{border: "none", textDecoration: "none"}} title={description}> {/**  @todo better implementation for desc showing **/}
-            <div className='flex flex-col items-center justify-center space-y-2 rounded-xl border-none  bg-stone-800 px-2 py-4 shadow-xl shadow-black/30 sm:p-6'>
-                <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{name}</h3> {/* Python Java JavaScript / TypeScript */}
+const StackBackend: React.FC<{ name: string}> = ({ name}) => (
+    <div className='p-2'>
+    <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{name}</h3> {/* Python Java JavaScript / TypeScript */}
+    <div className='mt-4 grid grid-rows-1 justify-center border-none'>
+        <div className='flex flex-wrap gap-4 md:gap-8 lg:gap-9
+                        rounded-xl border-none  bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                        shadow-xl shadow-black/30'>
+
+            <div className='border-none  bg-stone-800'>
+                <Icons.PythonIcon size_min={16} size_standard={32}></Icons.PythonIcon>
             </div>
-        </abbr>
+            <div className='border-none  bg-stone-800'>
+                <Icons.JavaScriptIcon size_min={16} size_standard={32}></Icons.JavaScriptIcon>
+            </div>
+            <div className='border-none  bg-stone-800'>
+                <Icons.TypeScriptIcon size_min={16} size_standard={32}></Icons.TypeScriptIcon>
+            </div>
+            <div className='border-none  bg-stone-800'>
+                <Icons.JavaIcon size_min={16} size_standard={32}></Icons.JavaIcon>
+            </div>
+            
+        </div>
     </div>
+</div>
 )
 
 
-{/** @todo: fix gap-size **/}
-const TestBlock: React.FC<{ title: string}> = ({ title}) => (
+const StackFrontend: React.FC<{ name: string}> = ({ name}) => (
     <div className='p-2'>
-        <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{title}</h3> {/* Python Java JavaScript / TypeScript */}
-        <div className='mt-4 grid grid-rows-1 border-none'>
-            <div className='flex flex-wrap gap-4 md:gap-8 lg:gap-9
-                            justify-items-start items-center rounded-xl border-none  bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
-                            shadow-xl shadow-black/30'>
+    <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{name}</h3> {/* Python Java JavaScript / TypeScript */}
+    <div className='mt-4 grid grid-rows-1 justify-center border-none'>
+        <div className='flex flex-wrap gap-4 md:gap-8 lg:gap-9
+                        rounded-xl border-none  bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                        shadow-xl shadow-black/30'>
 
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
-                <div className='border-none  bg-stone-800'>
-                    <PythonIcon size_min={16} size_standard={32}></PythonIcon>
-                </div>
+            <div className='border-none  bg-stone-800'>
+                <Icons.ReactIcon size_min={16} size_standard={32}></Icons.ReactIcon>
             </div>
+            <div className='border-none  bg-stone-800'>
+                <Icons.TailwindIcon size_min={16} size_standard={32}></Icons.TailwindIcon>
+            </div>
+            
         </div>
     </div>
-);
+</div>
+)
+
+
+const StackPrivacy: React.FC<{ name: string}> = ({ name}) => (
+    <div className='p-2'>
+    <h3 className='text-md text-center font-extrabold tracking-tight text-stone-200 sm:text-lg'>{name}</h3> {/* Python Java JavaScript / TypeScript */}
+    <div className='mt-4 grid grid-rows-1 justify-center border-none'>
+        <div className='flex flex-wrap gap-4 md:gap-8 lg:gap-9
+                        rounded-xl border-none  bg-stone-800 bg-stone-800 px-1 py-2 sm:px-2 sm:py-4 
+                        shadow-xl shadow-black/30'>
+
+            <div className='border-none  bg-stone-800'>
+                <Icons.PythonIcon size_min={16} size_standard={32}></Icons.PythonIcon>
+            </div>
+            {/** @todo: */}
+            
+        </div>
+    </div>
+</div>
+)
 
 
 function StackNew() {
@@ -93,9 +83,9 @@ function StackNew() {
 
             <div className='grid'>
                 <div className='space-y-2 rounded-xl border-none  px-2 py-4 shadow-xl shadow-black/30 sm:p-6'> {/** @todo: padding size**/}
-                    <TestBlock title="Backend"></TestBlock>
-                    <TestBlock title="Frontend"></TestBlock>
-                    <TestBlock title="Privacy"></TestBlock>
+                    <StackBackend name="Backend"></StackBackend>
+                    <StackFrontend name="Frontend"></StackFrontend>
+                    <StackPrivacy name="Privacy"></StackPrivacy>
                 </div>
             </div>
         </section>
