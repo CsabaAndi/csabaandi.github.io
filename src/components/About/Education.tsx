83eb3645d7b760eec20cb281edea4e4bba76473a
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-12 19:22:52 +0200

    [UI] Changed Language grid alignement on lg screen

--- src/components/About/Education.tsx
+++ src/components/About/Education.tsx
@@ -21,25 +21,25 @@ const ExperienceRowBlock: React.FC<experience> = (data: experience) => (
 {/** @todo: space beetween edu/lang **/}
 function Education() {
     return (
-        <section id='sect_4_edu' className='px-4 mb-12 sm:p-0 sm:px-4 md:px-8'> {/** @todo: padding/margin size**/}
-            <div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-48'>
+        <section id='sect_4_edu' className='px-4 mb-12 sm:p-0 sm:px-4 md:px-8'> {/** @todo: padding/margin size  px-4 mb-12 sm:p-0 sm:px-4 md:px-8**/}
+            <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
                 <div className='col-span-1'>
                     <h2 className='pb-4 sm:pb-6 text-2xl font-extrabold tracking-tight text-stone-200 sm:text-3xl'>
                         <span className='text-green-700'>Tan</span>
                         ulmányok
                     </h2>
-                    <ul className="md:ml-0 lg:ml-0">
+                    <ul className="md:ml-4 lg:ml-0 lg:px-8">
                         <ExperienceRowBlock start_year={2019} end_year="present" location="Szegedi Tudományegyetem" description="Bachelor of Computer Science Engineering"></ExperienceRowBlock>  
                         <ExperienceRowBlock start_year={2015} end_year={2019} location="Szekszárdi I. Béla Gimnázium, Kollégium és Általános Iskola" description="Érettségi"></ExperienceRowBlock>                    
                     </ul>
                 </div>
-                <div className='col-span-1'>
+                <div className='col-span-1 lg:px-12 lg:justify-self-end'>
                     <h2 className='pb-6 text-2xl font-extrabold tracking-tight text-stone-200 sm:text-3xl'>
                         <span className='text-green-700'>Ny</span>
                         elv
                     </h2>
-                    <ul className="md:ml-0">                       
-                        <ExperienceRowBlock start_year={2019} end_year="" location="Szekszárd" description="English vmi complex c"></ExperienceRowBlock>               
+                    <ul className="md:ml-4 lg:ml-0 lg:px-8">                       
+                        <ExperienceRowBlock start_year={2019} end_year="" location="Szekszárd" description="Angol C tipusú nyelvizsga"></ExperienceRowBlock>               
                     </ul>
                 </div>
                 {/* for future work experience */}
