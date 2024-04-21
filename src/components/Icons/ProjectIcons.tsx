c7a496925abc6b6327bdd777686f7a0d7268f72f
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-21 20:54:00 +0200

    Added Firebase icon

--- src/components/Icons/ProjectIcons.tsx
+++ src/components/Icons/ProjectIcons.tsx
@@ -259,3 +259,24 @@ export function NodeJsIcon(props: IconSizeProps){
         </div>
     )
 }
+
+
+
+export function FirebaseIcon(props: IconSizeProps){
+    return (
+        <div>
+            <div className='flex gap-x-2 sm:gap-x-3 sm:hidden'>
+                <a href='https://firebase.google.com/' target="_blank" rel="noopener noreferrer" title="Firebase">
+                    {/*LIcense - https://iconscout.com/free-icon/firebase-3521427 | by:  Icon 54 */}
+                    <svg xmlns="http://www.w3.org/2000/svg" width={props.size_min} height={props.size_min} viewBox="0 0 24 24" id="firebase"><path fill="#FFA000" d="m14.714 8.669-2.4 2.235-2.228-4.496 1.151-2.585c.291-.516.767-.522 1.058 0l2.419 4.846z"></path><path fill="#F57F17" d="m12.314 10.903-8.979 8.351 6.751-12.846 2.228 4.495z"></path><path fill="#FFCA28" d="M17.346 5.251c.43-.41.873-.271.985.31l2.334 13.58-7.742 4.648c-.272.152-.992.211-.992.211s-.655-.08-.906-.218l-7.689-4.528 14.01-14.003z"></path><path fill="#FFA000" d="m10.086 6.408-6.75 12.846L6.344.477c.113-.582.443-.641.74-.126l3.002 6.057z"></path></svg>
+                </a>
+            </div>
+            <div className='flex gap-x-2 sm:gap-x-3 hidden sm:flex'>
+                <a href='https://firebase.google.com/' target="_blank" rel="noopener noreferrer" title="Firebase">
+                    {/*License - https://iconscout.com/free-icon/firebase-3521427 | by:  Icon 54 */}
+                    <svg xmlns="http://www.w3.org/2000/svg" width={props.size_standard} height={props.size_standard} viewBox="0 0 24 24" id="firebase"><path fill="#FFA000" d="m14.714 8.669-2.4 2.235-2.228-4.496 1.151-2.585c.291-.516.767-.522 1.058 0l2.419 4.846z"></path><path fill="#F57F17" d="m12.314 10.903-8.979 8.351 6.751-12.846 2.228 4.495z"></path><path fill="#FFCA28" d="M17.346 5.251c.43-.41.873-.271.985.31l2.334 13.58-7.742 4.648c-.272.152-.992.211-.992.211s-.655-.08-.906-.218l-7.689-4.528 14.01-14.003z"></path><path fill="#FFA000" d="m10.086 6.408-6.75 12.846L6.344.477c.113-.582.443-.641.74-.126l3.002 6.057z"></path></svg>
+                </a>
+            </div>
+        </div>
+    )
+}
