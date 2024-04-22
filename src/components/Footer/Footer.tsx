72986dc6430355d4bfe05b0049ff1a00e1f76ee2
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-23 01:17:26 +0200

    Updated text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ import { EmailIcon, GithubIcon, LinkedinIcon } from "../Icons/SocialIcons"
 function Footer(){
         return (
           <footer className='flex flex-col w-full items-center justify-between gap-y-2 bg-stone-800 py-2 sm:p-4 sm:flex-row'>
-          <p id="footer_p_copyright" className='text-sm font-semibold text-stone-300'>© 2023 Csaba Andi</p>
+          <p id="footer_p_copyright" className='text-sm font-semibold text-stone-300'>© 2024 Andi Csaba</p>
           <div id="footer_icons" className='flex gap-x-2 sm:gap-x-4 text-stone-200'>
             <GithubIcon size_min={20} size_standard={20} link="https://github.com/CsabaAndi"></GithubIcon>
             <LinkedinIcon size_min={20} size_standard={20}></LinkedinIcon>
