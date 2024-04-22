e14a87792b5bf82d9dec79a80966d8cf92de2551
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-23 01:10:02 +0200

    Changed name color | changed margins/paddings

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,6 @@
 import { GithubIcon, LinkedinIcon} from '../Icons/SocialIcons';
 
 
-// border border-stone-900 base
-// border border-sky-600 testing
-
-    
 function Header() {
 
     const scrollToSection = (section: string) => {
@@ -19,22 +15,22 @@ function Header() {
 
 
     return (
-        <div className='flex w-full items-center justify-between border border-stone-900 p-4 md:p-8'>
-            <text id="header_text_name" className='flex items-center text-xs text-white sm:text-3xl'>
-                <span className='mr-1'>Andi</span>
-                <span className='font-bold'>Csaba</span>
+        <div className='flex w-full items-center justify-between border border-stone-900 mt-2 mb-6 px-4 sm:mt-2 sm:px-4 sm:mb-12 md:mt-4 md:px-8 md:mb-12 lg:mt-4 xl:mt-6 2xl:mt-8'>
+            <text id="header_text_name" className='flex items-center text-sm text-white sm:text-xl md:text-2xl lg:text-3xl'>
+                <span className='mr-0.5 sm:mr-1'>Andi</span>
+                <span className='font-bold text-green-600'>Csaba</span>
             </text>
-            <div id="header_section_links" className='flex gap-x-2 text-stone-200 text-xs sm:gap-x-4 sm:text-2xl'> {/** @todo: better implementation pls **/}
+            <div id="header_section_links" className='flex gap-x-2 text-stone-200 text-xs sm:gap-x-4 sm:text-lg lg:text-2xl'> {/** @todo: better implementation pls **/}
                 <button onClick={() => scrollToSection("sect_xy_thesis")}>
                     <span>Szakdolgozat</span>
                 </button>
-                <button onClick={() => scrollToSection("sect_4_edu")}>
-                    <span>Tanulmányok</span>
+                <button onClick={() => scrollToSection("sect_3_stacknew")}>
+                    <span>Ismeretek</span>
                 </button>
             </div>
             <div id="header_icons" className='flex gap-x-2 sm:gap-x-3'>
-                <GithubIcon size_min={16} size_standard={32} link='https://github.com/CsabaAndi'></GithubIcon>
-                <LinkedinIcon size_min={16} size_standard={32}></LinkedinIcon>
+                <GithubIcon size_min={16} size_standard={26} link='https://github.com/CsabaAndi'></GithubIcon>
+                <LinkedinIcon size_min={16} size_standard={26}></LinkedinIcon>
             </div>
         </div>
         )
