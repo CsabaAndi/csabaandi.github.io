de185e66ff67bd2670a9e3a63dcadc9b17b7b70d
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-23 02:10:05 +0200

    Added more custom screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
       screens: {
         'xxs': '459px',
         'xs': '465px',
+        'xxl': '1360px',
+        'xxl2': '1420px',
       },
       fontSize: {
         'tiny': '0.5rem', // 10px
